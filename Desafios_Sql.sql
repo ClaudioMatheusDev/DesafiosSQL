28c04be3f81b45420d0b21233c22277a7fda6568
Claudio Matheus <165329797+ClaudioMatheusDev@users.noreply.github.com>
2024-10-01 21:53:31 -0300

    Update Desafios_Sql.sql

--- Desafios_Sql.sql
+++ Desafios_Sql.sql
@@ -2,6 +2,7 @@ CREATE DATABASE Desafios_Sql;
 
 USE Desafios_Sql;
 
+-- DESAFIO 1
 
 CREATE TABLE Clientes(
 Id_Cliente INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -12,5 +13,51 @@ Cidade VARCHAR(100) NOT NULL
 
 SELECT * FROM Clientes WHERE Cidade ='São Paulo';
 
+-- DESAFIO 2
+CREATE TABLE Pedidos(
+  Id_Pedido INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  Id_Cliente INT NOT NULL,
+  FOREIGN KEY(Id_Cliente) REFERENCES Cliente(Id_Cliente),
+  Data DATETIME NOT NULL,
+  Valor DECIMAL(10, 2) NOT NULL
+);
+
+SELECT Id_Cliente, COUNT(*) AS Todos_Pedidos
+FROM Pedidos
+GROUP BY Id_Cliente;
+
+-- DESAFIO 3
+
+CREATE TABLE Produtos(
+  Id_Produto INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  Nome VARCHAR(100) NOT NULL,
+  Preco DECIMAL(10, 2) NOT NULL, 
+  Categoria VARCHAR(100) NOT NULL
+);
+
+SELECT Id_Produto, Nome, Preco AS Valor_Maior_100
+FROM Produtos
+WHERE Preco > 100
+GROUP BY Id_Produto;
+
+-- DESAFIO 4
+
+SELECT c.Nome, p.Valor
+FROM Clientes c
+JOIN Pedidos p ON c.Id_Cliente = p.Id_Cliente;
 
 
+-- DESAFIO 5
+
+CREATE TABLE Vendas(
+Id_Venda INT PRIMARY KEY AUTO_INCREMENT NOT NULL,  
+Id_Produto INT NOT NULL,
+FOREIGN KEY(Id_Produto) REFERENCES Produtos(Id_Produto),
+Valor DECIMAL(10, 2) NOT NULL,
+Quantidade INT NOT NULL
+);
+
+SELECT v.Id_Produto, SUM(v.Quantidade) AS Total_Vendido
+FROM Vendas v
+GROUP BY v.Id_Produto;
+ 
